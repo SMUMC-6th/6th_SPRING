36b279c0179a21ede2cc4241dc9769c30873a714
Jamey <musoyou1085@gmail.com>
2024-03-19 17:31:28 +0900

    Chore: 초기 설정 - git ignore 설정

---
+++ src/main/java/com/example/umc/study/UmcStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.umc.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmcStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmcStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/umc/study/UmcStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.umc.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmcStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
