804a9e479a5a0f741dae11baf1be7b9bc835ef84
chamm99 <alslalsl3125@gmail.com>
2024-03-29 03:52:07 +0900

    :sparkles: Feat: 엔티티 생성 및 스웨거 설정 - 엔티티 생성 - 스웨거 설정

---
+++ src/main/java/com/example/umc/study/domain/Category.java
@@ -0,0 +1,28 @@
+package com.example.umc.study.domain;
+
+import com.example.umc.study.domain.mapping.PostCategory;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "category_id")
+    private Long id;
+
+    private String title;
+
+    @OneToMany(mappedBy = "category")
+    private List<PostCategory> postCategories = new ArrayList<>();
+}
---
+++ src/main/java/com/example/umc/study/domain/Post.java
@@ -0,0 +1,32 @@
+package com.example.umc.study.domain;
+import com.example.umc.study.domain.mapping.PostCategory;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "post_id")
+    private Long id;
+
+    private String title;
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @OneToMany(mappedBy = "post")
+    private List<PostCategory> postCategories = new ArrayList<>();
+
+}
