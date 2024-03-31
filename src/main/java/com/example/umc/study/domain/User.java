e0799ce8e95de19c788e1ece59a6c5e9159e0d63
pse10307 <eum7548920@gmail.com>
2024-03-31 16:19:51 +0900

    :sparkles: Feat: 엔티티 생성 및 스웨거 설정 - 엔티티 생성 - 스웨거 설정

---
+++ src/main/java/com/example/umc/study/domain/User.java
@@ -0,0 +1,27 @@
+package com.example.umc.study.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+@Getter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "user")
+    private List<Post> posts = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user")
+    private List<Reply> replies = new ArrayList<>();
+}
