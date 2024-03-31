ef8231cfa5eb678725e71b2aee721e1af4e93f46
jinho7 <69239293+jinho7@users.noreply.github.com>
2024-04-01 03:07:30 +0900

    :sparkles: Feat: 엔티티 생성 스웨거 설정 - 엔티티 생성 - 스웨거 설정

---
+++ src/main/java/com/example/umc/study/domain/Reply.java
@@ -0,0 +1,32 @@
+package com.example.umc.study.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class Reply {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "reply_id")
+    private Long id;
+
+    private String title;
+
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+}
