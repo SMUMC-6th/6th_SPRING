c16a7d9f13404000e7e33e81a55ff153f5285fdb
ucaeon <yoocw0426@gmail.com>
2024-04-01 16:47:25 +0900

    :sparkles: Feat: 엔티티 생성 및 스웨거 설정 엔티티 생성 스웨거 설정

---
+++ src/main/java/com/example/umc/study/domain/Category.java
@@ -0,0 +1,26 @@
+package com.example.umc.study.domain;
+
+import com.example.umc.study.domain.mapping.PostCategory;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+@Getter
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "category_id")
+    private Long id;
+
+    private String title;
+
+    @OneToMany(mappedBy = "category")
+    private List<PostCategory> postCategories = new ArrayList<>();
+}
