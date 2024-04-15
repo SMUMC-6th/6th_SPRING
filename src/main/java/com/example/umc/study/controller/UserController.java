7d1ad88e0c8c89577fb8b3d2b1e09d6a0a868235
jiiiiiw <kijiwi1@naver.com>
2024-04-15 15:19:53 +0900

    :sparkles: Feat: User crd 구현

---
+++ src/main/java/com/example/umc/study/controller/UserController.java
@@ -0,0 +1,42 @@
+package com.example.umc.study.controller;
+
+import com.example.umc.study.service.UserService;
+import com.example.umc.study.apiPayload.BaseResponse;
+import com.example.umc.study.converter.UserConverter;
+import com.example.umc.study.domain.User;
+import com.example.umc.study.dto.UserRequestDTO;
+import com.example.umc.study.dto.UserResponseDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/api/v1/users")
+    public BaseResponse<UserResponseDTO.JoinResultDTO> createUser(@RequestBody UserRequestDTO.JoinDTO joinDTO){
+        User user = userService.createUser(joinDTO);
+        return BaseResponse.onSuccess(UserConverter.toJoinResultDTO(user));
+    }
+
+    @GetMapping("/api/v1/users/{userId}")
+    public BaseResponse<UserResponseDTO.UserPreviewDTO> readUser(@PathVariable Long userId){
+        User user = userService.readUser(userId);
+        return BaseResponse.onSuccess(UserConverter.toUserPreviewDTO(user));
+    }
+
+    @GetMapping("/api/v1/users")
+    public BaseResponse<UserResponseDTO.UserPreviewListDTO> readUsers(){
+        List<User> userList = userService.readUsers();
+        return BaseResponse.onSuccess(UserConverter.toUserPreviewListDTO(userList));
+    }
+
+    @DeleteMapping("/api/v1/users/{userId}")
+    public void deleteUser(@PathVariable Long userId){
+        userService.deleteUser(userId);
+    }
+}
