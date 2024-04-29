f63fc0c770d5e475aca41554573d21952ccc44be
HyunwooYi <83715335+HyunwooYi@users.noreply.github.com>
2024-04-29 14:39:57 +0900

    Transactional 어노테이션 추가

--- src/main/java/com/example/umc/study/service/PostServiceImpl.java
+++ src/main/java/com/example/umc/study/service/PostServiceImpl.java
@@ -8,6 +8,7 @@ import com.example.umc.study.dto.PostRequestDTO;
 import com.example.umc.study.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -17,22 +18,26 @@ public class PostServiceImpl implements PostService {
 
     private final PostRepository postRepository;
 
+    @Transactional
     @Override
     public Post createPost(PostRequestDTO.JoinDTO joinDTO) {
         Post post = PostConverter.toPost(joinDTO);
         return postRepository.save(post);
     }
 
+    @Transactional(readOnly = true)
     @Override
     public Post readPost(Long postId) {
         return postRepository.findById(postId).orElseThrow(()-> new PostHandler(ErrorStatus._NOT_FOUND_POST));
     }
 
+    @Transactional(readOnly = true)
     @Override
     public List<Post> readPosts() {
         return postRepository.findAll();
     }
 
+    @Transactional
     @Override
     public void deletePost(Long postId) {
         Post post = postRepository.findById(postId).orElseThrow(()-> new PostHandler(ErrorStatus._NOT_FOUND_POST));
--- src/main/java/com/example/umc/study/service/ReplyServiceImpl.java
+++ src/main/java/com/example/umc/study/service/ReplyServiceImpl.java
@@ -8,6 +8,7 @@ import com.example.umc.study.dto.ReplyRequestDTO;
 import com.example.umc.study.repository.ReplyRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -17,22 +18,24 @@ public class ReplyServiceImpl implements ReplyService{
 
     private final ReplyRepository replyRepository;
 
+    @Transactional
     @Override
     public Reply createReply(ReplyRequestDTO.JoinDTO joinDTO) {
         Reply reply = ReplyConverter.toReply(joinDTO);
         return replyRepository.save(reply);
     }
 
+    @Transactional(readOnly = true)
     @Override
     public Reply readReply(Long replyId) {
         return replyRepository.findById(replyId).orElseThrow(()->new ReplyHandler(ErrorStatus._NOT_FOUND_REPLY));
     }
-
+    @Transactional(readOnly = true)
     @Override
     public List<Reply> readReplys() {
         return replyRepository.findAll();
     }
-
+    @Transactional
     @Override
     public void deletePost(Long replyId) {
         Reply reply = replyRepository.findById(replyId).orElseThrow(()-> new ReplyHandler(ErrorStatus._NOT_FOUND_REPLY));
