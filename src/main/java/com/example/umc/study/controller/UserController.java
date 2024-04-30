20d090121e70157aa29f89e3bb5adf353689a61d
kimtaeyoung <62414583+kimtaeyoung201910794@users.noreply.github.com>
2024-04-30 18:03:16 +0900

    :sparkles: Feat: CRUD 중 CRD 기능 구현 - CRUD 중 CRD 기능 구현 - 응답 통일 설정

---
+++ src/main/java/com/example/umc/study/controller/UserController.java
@@ -0,0 +1,46 @@
+package com.example.umc.study.controller;
+
+import com.example.umc.study.apiPayload.BaseResponse;
+import com.example.umc.study.converter.UserConverter;
+import com.example.umc.study.domain.User;
+import com.example.umc.study.dto.UserRequestDTO;
+import com.example.umc.study.dto.UserResponseDTO;
+import com.example.umc.study.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    //회원 등록
+    @PostMapping("/api/v1/users")
+    public BaseResponse<UserResponseDTO.JoinResultDTO> createUser(@RequestBody UserRequestDTO.JoinDTO joinDTO) {
+        User user = userService.createUser(joinDTO);
+        return BaseResponse.onSuccess(UserConverter.toJoinResultDTO(user));
+    }
+
+    //회원 id로 조회
+    @GetMapping("/api/v1/users/{userId}")
+    public BaseResponse<UserResponseDTO.UserPreviewDTO> readUser(@PathVariable Long userId) {
+        User user = userService.readUser(userId);
+        return BaseResponse.onSuccess(UserConverter.toUserPreviewDTO(user));
+    }
+
+    //회원 전체조회
+    @GetMapping("/api/v1/users")
+    public BaseResponse<UserResponseDTO.UserPreviewListDTO> readUsers() {
+        List<User> userList = userService.readUsers();
+        return BaseResponse.onSuccess(UserConverter.toUserPreviewListDTO(userList));
+    }
+
+    //회원 id로 삭제
+    @DeleteMapping("/api/v1/users/{userId}")
+    public void deleteUser(@PathVariable Long userId) {
+        userService.deleteUser(userId);
+    }
+}
---
+++ src/main/java/com/example/umc/study/dto/UserRequestDTO.java
@@ -0,0 +1,12 @@
+package com.example.umc.study.dto;
+
+import lombok.Getter;
+
+public class UserRequestDTO {
+
+    @Getter
+    public static class JoinDTO {
+
+        private String name;
+    }
+}
