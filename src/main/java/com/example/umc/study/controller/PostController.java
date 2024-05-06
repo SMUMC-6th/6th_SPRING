fe7477aa3bfcb05bd00adc73cc7088b55fc3c67d
jiiiiiw <kijiwi1@naver.com>
2024-05-06 18:02:00 +0900

    :sparkles: Feat: CORS 에러 해결 구현

--- src/main/java/com/example/umc/study/controller/PostController.java
+++ src/main/java/com/example/umc/study/controller/PostController.java
@@ -14,6 +14,7 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("api/v1")
+@CrossOrigin("*")
 public class PostController {
 
     private final PostService postService;
--- src/main/java/com/example/umc/study/controller/ReplyController.java
+++ src/main/java/com/example/umc/study/controller/ReplyController.java
@@ -14,6 +14,7 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("api/v1")
+@CrossOrigin("*")
 public class ReplyController {
 
     private final ReplyService replyService;
