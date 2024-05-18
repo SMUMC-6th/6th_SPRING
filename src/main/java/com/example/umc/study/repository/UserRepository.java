cedf0e6655530a5643f34b1e484b4a8d22d5bdd5
gomin0 <gomin0981113@naver.com>
2024-05-18 16:11:15 +0900

    :sparkles: feat: Spring Security 인증 인가 처리, password 암호화 기능 추가 - UserDetails 구현 - filterChain 추가 - passwordEncoder 추가

--- src/main/java/com/example/umc/study/repository/UserRepository.java
+++ src/main/java/com/example/umc/study/repository/UserRepository.java
@@ -5,6 +5,8 @@ import com.example.umc.study.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
 }
