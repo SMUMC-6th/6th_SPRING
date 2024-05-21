3808c13675897a4414b156d8167f4c7d56206cc9
jiiiiiw <kijiwi1@naver.com>
2024-05-22 03:27:48 +0900

    :sparkles: Feat: Spring Security 인증 인가 처리, password 암호화 기능 추가

--- src/main/java/com/example/umc/study/repository/UserRepository.java
+++ src/main/java/com/example/umc/study/repository/UserRepository.java
@@ -3,6 +3,8 @@ package com.example.umc.study.repository;
 import com.example.umc.study.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+import java.util.Optional;
 
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
 }
