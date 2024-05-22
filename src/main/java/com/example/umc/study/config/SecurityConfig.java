d862fb88072ed30444d6fb0aa6706be7cdf57ee3
moonyaeyoon <moonyaeyoon@moonyaeyoon.com>
2024-05-22 12:03:15 +0900

    :bug: Fix : 불필요한 라이브러리 삭제

--- src/main/java/com/example/umc/study/config/SecurityConfig.java
+++ src/main/java/com/example/umc/study/config/SecurityConfig.java
@@ -1,14 +1,12 @@
 package com.example.umc.study.config;
 
-import org.springframework.boot.autoconfigure.security.servlet.SecurityFilterAutoConfiguration;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
