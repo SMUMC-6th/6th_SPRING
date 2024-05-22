658398696c377691687a4b72938c5b6f3e7dc91c
Jamey <musoyou1085@gmail.com>
2024-05-22 17:10:52 +0900

    :sparkles: Feat: 인가, 필터 실습 - role 추가 - filter 추가

--- src/main/java/com/example/umc/study/config/SecurityConfig.java
+++ src/main/java/com/example/umc/study/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.umc.study.config;
 
+import com.example.umc.study.config.filter.TestFilter;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,12 +14,19 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.intercept.AuthorizationFilter;
 import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+import org.springframework.security.web.authentication.AnonymousAuthenticationFilter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.ui.DefaultLoginPageGeneratingFilter;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Collections;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
+@EnableWebSecurity(debug = true)
 public class SecurityConfig {
 
     private final String[] allowUrl = {
@@ -31,11 +39,25 @@ public class SecurityConfig {
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.cors((c)->c.configurationSource(request -> {
+            CorsConfiguration config = new CorsConfiguration();
+            config.setAllowedOrigins(Collections.singletonList("*"));
+            config.setAllowedMethods(Collections.singletonList("*"));
+            config.setAllowCredentials(true);
+            config.setAllowedHeaders(Collections.singletonList("*"));
+            config.setMaxAge(3600L);
+            return config;
+        }));
+
         http.csrf(AbstractHttpConfigurer::disable);
         http.authorizeHttpRequests((requests) -> requests
                 .requestMatchers(HttpMethod.POST,"/api/v1/users").permitAll()
+                .requestMatchers(HttpMethod.POST,"/api/v1/users/{userId}/posts").hasAnyRole("USER", "ADMIN")
+                .requestMatchers(HttpMethod.POST,"/api/v1/replies").hasRole( "ADMIN")
                 .requestMatchers(allowUrl).permitAll()
                 .anyRequest().authenticated());
+
+        http.addFilterAfter(new TestFilter(), BasicAuthenticationFilter.class);
 
         http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
