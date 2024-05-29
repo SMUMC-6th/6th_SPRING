d7ab602e3a4342df5c04d67c3c54cdc775e8ca31
moonyaeyoon <moonyaeyoon@moonyaeyoon.com>
2024-05-29 10:37:04 +0900

    :sparkles: Feat : CustomFilter 생성 -testFilter 생성 -선택된 FilterChain 출력 -로그인 안 한 user인 경우 anonymousUser 출력

--- src/main/java/com/example/umc/study/config/SecurityConfig.java
+++ src/main/java/com/example/umc/study/config/SecurityConfig.java
@@ -1,15 +1,18 @@
 package com.example.umc.study.config;
 
 
+import com.example.umc.study.config.filter.TestFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AnonymousAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 
 import java.util.Collections;
@@ -18,6 +21,7 @@ import static org.springframework.security.config.Customizer.withDefaults;
 
 
 @Configuration
+@EnableWebSecurity(debug = true)
 public class SecurityConfig {
 
     private final String[] allowUrl = {
@@ -54,6 +58,8 @@ public class SecurityConfig {
                         .anyRequest().authenticated());
         http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
+        http.addFilterAfter(new TestFilter(), AnonymousAuthenticationFilter.class);
+
         return http.build();
     }
 }
