af458e9160c355a172e8e1e9bcf4596a81e2340b
jinnieusLab <kitty14904@naver.com>
2024-06-16 18:32:37 +0900

    :sparkles:   Feat: JWT

--- src/main/java/com/example/umc/study/config/SecurityConfig.java
+++ src/main/java/com/example/umc/study/config/SecurityConfig.java
@@ -1,29 +1,57 @@
 package com.example.umc.study.config;
 
 import com.example.umc.study.config.filter.TestFilter2;
+import com.example.umc.study.config.filter.jwt.JWTUtil;
+import com.example.umc.study.config.filter.jwt.JwtAccessDeniedHandler;
+import com.example.umc.study.config.filter.jwt.filter.JWTExceptionFilter;
+import com.example.umc.study.config.filter.jwt.filter.JWTFilter;
+import com.example.umc.study.config.filter.jwt.filter.JwtAuthenticationEntryPoint;
+import com.example.umc.study.config.filter.jwt.filter.LoginFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.expression.DefaultHttpSecurityExpressionHandler;
 import org.springframework.security.web.authentication.AnonymousAuthenticationFilter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
-@EnableWebSecurity(debug = true)
+@RequiredArgsConstructor
 public class SecurityConfig {
+    private final AuthenticationConfiguration authenticationConfiguration;
+    private final JWTUtil jwtUtil;
+    private final PrincipalDetailsService principalDetailsService;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+
     private final String[] allowUrl = {
             "/swagger-ui/**",
             "/swagger-resources/**",
             "/v3/api-docs/**",
             "/api/v1/posts/**",
-            "/api/v1/replies/**"
+            "/api/v1/replies/**",
+            "/login"
     };
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
@@ -35,10 +63,22 @@ public class SecurityConfig {
                 .requestMatchers(HttpMethod.POST,"/api/v1/users/{userId}/posts/{postId}/replies").hasRole("ADMIN")
                 .requestMatchers(HttpMethod.PATCH,"/api/v1/replies/{replyId}").hasRole("ADMIN")
                 .requestMatchers(allowUrl).permitAll()
-                .anyRequest().authenticated()); // 모든 요청에 대해 인증을 하게 함.
-        http.formLogin(withDefaults());
-        http.httpBasic(withDefaults());
-        http.addFilterAfter(new TestFilter2(), AnonymousAuthenticationFilter.class);
+                .anyRequest().authenticated()) // 모든 요청에 대해 인증을 하게 함.
+            .addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration), jwtUtil), UsernamePasswordAuthenticationFilter.class)
+            .addFilterBefore(new JWTFilter(jwtUtil, principalDetailsService), UsernamePasswordAuthenticationFilter.class)
+            .addFilterBefore(new JWTExceptionFilter(), JWTFilter.class);
+
+        http.exceptionHandling(
+                (configurer ->
+                        configurer.authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                                .accessDeniedHandler(jwtAccessDeniedHandler)
+                )
+        );
+
+        http.formLogin(AbstractHttpConfigurer::disable); // form 로그인 대신 JSON으로 로그인 받기
+        http.httpBasic(AbstractHttpConfigurer::disable);
+        http.sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+//        http.addFilterAfter(new TestFilter2(), AnonymousAuthenticationFilter.class);
         return http.build();
     }
 
---
+++ src/main/java/com/example/umc/study/config/filter/jwt/filter/LoginFilter.java
@@ -0,0 +1,85 @@
+package com.example.umc.study.config.filter.jwt.filter;
+
+import com.example.umc.study.apiPayload.BaseResponse;
+import com.example.umc.study.apiPayload.code.status.ErrorStatus;
+import com.example.umc.study.apiPayload.exception.handler.AuthHandler;
+import com.example.umc.study.config.PrincipalDetails;
+import com.example.umc.study.config.filter.jwt.JWTUtil;
+import com.example.umc.study.dto.request.LoginRequestDTO;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Iterator;
+
+@RequiredArgsConstructor
+public class LoginFilter extends UsernamePasswordAuthenticationFilter {
+    private final AuthenticationManager authenticationManager;
+    private final JWTUtil jwtUtil;
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        LoginRequestDTO loginRequestDTO = readBody(request);
+
+        UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = UsernamePasswordAuthenticationToken.unauthenticated(loginRequestDTO.getEmail(), loginRequestDTO.getPassword());
+
+        return authenticationManager.authenticate(usernamePasswordAuthenticationToken);
+    }
+
+    private LoginRequestDTO readBody(HttpServletRequest request) {
+        LoginRequestDTO loginRequestDTO = null;
+        ObjectMapper om = new ObjectMapper();
+
+        try {
+            loginRequestDTO = om.readValue(request.getInputStream(), LoginRequestDTO.class);
+        } catch (IOException e) {
+            throw new AuthHandler(ErrorStatus._BAD_REQUEST);
+        }
+        return loginRequestDTO;
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+        PrincipalDetails principalDetails = (PrincipalDetails) authResult.getPrincipal();
+
+        String email = principalDetails.getUsername();
+        Collection<? extends GrantedAuthority> authorities = authResult.getAuthorities();
+        Iterator<? extends GrantedAuthority> iterator = authorities.iterator();
+        GrantedAuthority auth = iterator.next();
+
+        String role = auth.getAuthority();
+        String token = jwtUtil.createAccessToken(email, role);
+        response.addHeader("Authorization", "Bearer " + token);
+        response.setStatus(HttpStatus.OK.value());
+
+        BaseResponse<Object> successResponse = BaseResponse.onSuccess(null);
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.writeValue(response.getOutputStream(), successResponse);
+    }
+
+    @Override
+    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) throws IOException, ServletException {
+        response.setContentType("application/json; charset=UTF-8");
+        response.setStatus(HttpStatus.UNAUTHORIZED.value()); // 401
+        ErrorStatus UN_AUTH = ErrorStatus._AUTHENTICATION_FAILED;
+
+        BaseResponse<Object> errorResponse =
+                BaseResponse.onFailure(UN_AUTH.getCode(), UN_AUTH.getMessage(), null);
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.writeValue(response.getOutputStream(), errorResponse);
+    }
+}
