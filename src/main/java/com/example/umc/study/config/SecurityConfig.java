c81ec0bbe7092a62e79a5a465b8d849e7d5bfda2
Jeongmo Seo <wjdah1402@gmail.com>
2024-06-19 13:36:48 +0900

    :sparkles: feat: Jwt 인증, 인가 구현 - Jwt 인증, 인가 구현

--- src/main/java/com/example/umc/study/config/SecurityConfig.java
+++ src/main/java/com/example/umc/study/config/SecurityConfig.java
@@ -1,29 +1,43 @@
 package com.example.umc.study.config;
 
-import com.example.umc.study.config.filter.TestFilter;
-import com.example.umc.study.config.filter.TestFilter2;
+import com.example.umc.study.config.filter.*;
+import com.example.umc.study.config.jwt.filter.JwtExceptionFilter;
+import com.example.umc.study.config.jwt.filter.JwtFilter;
+import com.example.umc.study.config.jwt.handler.JwtAccessDeniedHandler;
+import com.example.umc.study.config.jwt.handler.JwtAuthenticationEntryPoint;
+import com.example.umc.study.util.JwtUtil;
 import jakarta.servlet.Filter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.FormLoginConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AnonymousAuthenticationFilter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
-import org.springframework.security.web.context.SecurityContextHolderFilter;
 import org.springframework.web.cors.CorsConfiguration;
 
 import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity(debug = true)
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final AuthenticationConfiguration authenticationConfiguration;
+    private final JwtUtil jwtUtil;
+    private final PrincipalDetailsService principalDetailsService;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
 
     private final String[] allowUrl = {
             "/swagger-ui/**",
@@ -38,6 +52,9 @@ public class SecurityConfig {
         http
                 .addFilterAfter(new TestFilter(), BasicAuthenticationFilter.class)
                 .addFilterAfter(testFilter2(), AnonymousAuthenticationFilter.class)
+                .addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration), jwtUtil), UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(new JwtFilter(jwtUtil, principalDetailsService), LoginFilter.class)
+                .addFilterBefore(new JwtExceptionFilter(), JwtFilter.class)
                 .cors(configurer -> configurer
                         .configurationSource(request -> {
                             CorsConfiguration config = new CorsConfiguration();
@@ -48,7 +65,11 @@ public class SecurityConfig {
                             config.setMaxAge(3600L);
                             return config;
                         }))
-                .csrf(AbstractHttpConfigurer::disable)
+                .exceptionHandling(configurer -> configurer
+                        .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                        .accessDeniedHandler(jwtAccessDeniedHandler)
+                )
+
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.POST, "/api/v1/users").permitAll()
                         .requestMatchers(HttpMethod.POST, "/api/v1/users/{userId}/posts").hasAnyRole("ADMIN", "USER")
@@ -57,8 +78,10 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.PATCH, "/api/v1/replies/{replyId}").hasRole("ADMIN")
                         .requestMatchers(allowUrl).permitAll()
                         .anyRequest().authenticated())
-                .formLogin(Customizer.withDefaults())
-                .httpBasic(Customizer.withDefaults());
+
+                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(FormLoginConfigurer::disable)
+                .httpBasic(HttpBasicConfigurer::disable);
 
         return http.build();
     }
@@ -88,5 +111,10 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception{
+        return configuration.getAuthenticationManager();
     }
 }
