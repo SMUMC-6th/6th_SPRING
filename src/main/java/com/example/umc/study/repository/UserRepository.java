fce5c6139269544370466e6ef7b0460ccc28befa
moonyaeyoon <moonyaeyoon@moonyaeyoon.com>
2024-06-30 15:39:52 -0400

    :sparkles: Feat : oAuth 구현

--- src/main/java/com/example/umc/study/repository/UserRepository.java
+++ src/main/java/com/example/umc/study/repository/UserRepository.java
@@ -8,5 +8,6 @@ import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<Object> findByEmail(String email);
+    Optional<User> findByEmail(String email);
+    Boolean existsByEmail(String email);
 }
